f2672080248d5b9c6ac640a65d7e8488d799d876
VivianaSierra <164040207+VivianaSierra@users.noreply.github.com>
2025-03-05 23:22:42 -0500

    Final one

--- Biblioteca/Biblioteca.java
+++ Biblioteca/Biblioteca.java
@@ -129,7 +129,52 @@ public class Biblioteca extends JFrame implements ActionListener {
         SwingUtilities.invokeLater(() -> new Biblioteca());
     }
 
- 
+    // clase libro, la bendita clase libro que tanto dolor de cabeza nos dio :)
+   class Libro {
+       private String titulo;
+       private String autor;
+       private String ISBN;
+       private boolean disponible;
+       // constructor
+       public Libro(String titulo, String autor, String ISBN, boolean disponible) {
+           this.titulo = titulo;
+           this.autor = autor;
+           this.ISBN = ISBN;
+           this.disponible = disponible;
+       }
+       // Getters y Setters
+       public String getTitulo() {
+           return titulo;
+       }
+       public void setTitulo(String titulo) {
+           this.titulo = titulo;
+       }
+       public String getAutor() {
+           return autor;
+       }
+       public void setAutor(String autor) {
+           this.autor = autor;
+       }
+       public String getISBN() {
+           return ISBN;
+       }
+       public void setISBN(String ISBN) {
+           this.ISBN = ISBN;
+       }
+       public boolean isDisponible() {
+           return disponible;
+       }
+       public void setDisponible(boolean disponible) {
+           this.disponible = disponible;
+       }
+       // metodo toString para mostrar la información del libro
+       @Override
+       public String toString() {
+           return "Libro{titulo='" + titulo + "', autor='" + autor + "', ISBN='" + 
+                   + "}";
+       }
+   }
+}
  
  
  
